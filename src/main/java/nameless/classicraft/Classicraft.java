1669039ed045cf926363c7f55361ce4e9e6558a8
glyceryl <494192175@qq.com>
2022-09-03 19:46:35 +0800

    设置干海带和紫颂果不恢复饱食度

--- src/main/java/nameless/classicraft/Classicraft.java
+++ src/main/java/nameless/classicraft/Classicraft.java
@@ -4,33 +4,43 @@ import nameless.classicraft.common.block.ModBlocks;
 import nameless.classicraft.common.block.entity.ModBlockEntities;
 import nameless.classicraft.common.item.ModItems;
 import nameless.classicraft.common.menu.ModMenuTypes;
-import nameless.classicraft.event.PlayerUseItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Nameless
  */
 @Mod(Classicraft.MOD_ID)
 public class Classicraft {
+
     public static final String MOD_ID = "classicraft";
     public static final CreativeModeTab TAB = new CreativeModeTab(MOD_ID) {
+        @NotNull
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.ROTTEN_FOOD.get());
         }
     };
 
+    @SuppressWarnings("deprecation")
     public Classicraft() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModBlockEntities.REGISTER.register(bus);
+        if (Items.DRIED_KELP.getFoodProperties() != null) {
+            Items.DRIED_KELP.getFoodProperties().nutrition = 0;
+            Items.DRIED_KELP.getFoodProperties().saturationModifier = 0.0F;
+        } else if (Items.CHORUS_FRUIT.getFoodProperties() != null) {
+            Items.CHORUS_FRUIT.getFoodProperties().nutrition = 0;
+            Items.CHORUS_FRUIT.getFoodProperties().saturationModifier = 0.0F;
+        }
         ModItems.REGISTER.register(bus);
-        ModMenuTypes.REGISTER.register(bus);
         ModBlocks.REGISTER.register(bus);
-        MinecraftForge.EVENT_BUS.register(new PlayerUseItem());
+        ModMenuTypes.REGISTER.register(bus);
+        ModBlockEntities.REGISTER.register(bus);
     }
+
 }
