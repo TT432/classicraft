d223176dfb46289f9436b6c7c6246e302a92dd12
glyceryl <494192175@qq.com>
2022-09-06 12:57:08 +0800

    修正容器名字不正常显示的问题

--- src/main/java/nameless/classicraft/common/block/entity/BasicMenuBlockEntity.java
+++ src/main/java/nameless/classicraft/common/block/entity/BasicMenuBlockEntity.java
@@ -3,35 +3,24 @@ package nameless.classicraft.common.block.entity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author DustW
  **/
 public abstract class BasicMenuBlockEntity extends BasicBlockEntity implements MenuProvider {
 
-    protected BasicMenuBlockEntity(BlockEntityType<?> pType, BlockPos pWorldPosition, BlockState pBlockState) {
-        super(pType, pWorldPosition, pBlockState);
+    protected BasicMenuBlockEntity(BlockEntityType<?> type, BlockPos worldPosition, BlockState blockState) {
+        super(type, worldPosition, blockState);
     }
 
+    @NotNull
     @Override
     public Component getDisplayName() {
-        return new TranslatableComponent(defaultName());
+        return new TranslatableComponent("container.classicraft.fridge");
     }
 
-    String name;
-
-    protected String defaultName() {
-        return name == null ? setDefaultName() : name;
-    }
-
-    protected String setDefaultName() {
-        ResourceLocation registryName = getType().getRegistryName();
-        if (registryName != null)
-            name = "container." + registryName.toString().replace("/", ".");
-        return name;
-    }
 }
